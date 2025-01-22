f1d5e8082f7c871fc7203d9e7e8dc9eda38cedd7
Amit Ofeck <Amitofeck1@gmail.com>
2025-01-22 10:47:59 +0200

    initial express server with ts

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to my TypeScript Express server!');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
