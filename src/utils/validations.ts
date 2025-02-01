14c803ea22d89b92b3eb08ac991612488faa3db2
ortal124 <ortal120400@gmail.com>
2025-02-01 21:49:23 +0200

    fix validation func

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,9 +1,9 @@
 export function validateMandatoryFields<T>(data: Partial<T>, mandatoryFields: (keyof T)[]): boolean {
-    mandatoryFields.forEach((field) => {
-        if (!data[field]) {
+    for (const field of mandatoryFields) {
+        if (data[field] === undefined || data[field] === null) {
             return false;
         }
-    });
+    };
 
     return true;
 }
