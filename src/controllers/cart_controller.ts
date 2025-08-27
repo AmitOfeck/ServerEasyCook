b7747c0470a73954842067d2e8da520b6aab2752
ortal124 <ortal120400@gmail.com>
2025-08-27 22:32:51 +0300

    add errors to address sproblems in cart checkout

--- src/controllers/cart_controller.ts
+++ src/controllers/cart_controller.ts
@@ -3,21 +3,30 @@ import * as CartService from '../services/cart_service';
 import * as ShoppingListService from '../services/shopping_list_service';
 import UserService from '../services/user_service';
 import { IShoppingList } from '../models/shopping_list_model';
+import { AppError } from '../utils/AppError';
 
 export async function getBestCart(req: Request, res: Response) {    
   try {
-    const userId = req.params.userId;
+    const userId = (req as any).userId
     const shoppingList: IShoppingList | null = await ShoppingListService.getList(userId)
+
+    if(!shoppingList) 
+      res.status(400).send({ error: 'User shopping list not found' });
 
     const user = await UserService.getUserById(userId)
     if(user?.addresses && user.addresses[0] && shoppingList) {
       const carts = await CartService.findCheapestCart(shoppingList, user?.addresses[0] );
       res.status(200).send(carts);
     } else {
-      res.status(400).send({ error: 'User address or shopping list not found' });
+      res.status(400).send({ error: 'User has no address' });
     }
   } catch (error) {
     console.error('Error in getBestCart:', error);
-    res.status(500).send({ error: 'Internal server error' });
+
+    if (error instanceof AppError) {
+      res.status(error.statusCode).send({ error: error.message });
+    } else {
+      res.status(500).send({ error: 'Internal server error' });
+    }
   }
 }
